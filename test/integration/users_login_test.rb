c6fb776c6095a4e2990953bed76af5e066633ba1
Szymon Sobecki <szymon_sobecki@szymonsobecki.local>
2018-11-04 19:06:00 +0100

    8.3 Logging out

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -20,7 +20,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert.alert-danger', false, 'Invalid email/password combination'
   end
 
-  test 'login with valid information' do
+  test 'login with valid information followed by logout' do
     get login_path
     post login_path, params: {
       session: {
@@ -28,10 +28,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
         password: 'password'
       }
     }
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_select 'a[href=?]', login_path, count: 0
     assert_select 'a[href=?]', logout_path
     assert_select 'a[href=?]', user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path, count: 0
+    assert_select 'a[href=?]', user_path(@user), count: 0
   end
 end
