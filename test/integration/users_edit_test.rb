6db4e27a4ff2219dd26909bc965b8a939e95c84f
Szymon Sobecki <szymon_sobecki@szymonsobecki.local>
2018-11-24 00:07:31 +0100

    10.1.3 Testing unsuccessful edits

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'unsuccessful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: {
+      user: {
+        name: '',
+        email: 'foo@invalid',
+        password: 'foo',
+        password_confirmation: 'bar'
+      }
+    }
+    assert_template 'users/edit'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert.alert-danger', 'The form contains 4 errors'
+    assert_select 'ul' do
+      assert_select 'li', "Name can't be blank"
+      assert_select 'li', 'Email is invalid'
+      assert_select 'li', "Password confirmation doesn't match Password"
+      assert_select 'li', 'Password is too short (minimum is 6 characters)'
+    end
+  end
+end
