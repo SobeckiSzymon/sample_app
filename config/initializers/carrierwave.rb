98aa6ac9d050bd7720b930963e9be1367efcd248
Szymon Sobecki <szymon_sobecki@szymonsobecki.local>
2019-02-10 15:06:33 +0100

    Change fog to carrierwave-aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,19 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-require 'carrierwave/orm/activerecord'
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
+# require 'carrierwave/orm/activerecord'
 
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      # Configuration for Amazon S3
-      provider: 'AWS',
-      aws_access_key_id: ENV['S3_ACCESS_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET_KEY'],
-      region: ENV['S3_REGION']
+    config.storage    = :aws
+    config.aws_bucket = ENV.fetch('S3_BUCKET') # for AWS-side bucket access permissions config, see section below
+    config.aws_acl    = 'public-read'
+
+    config.aws_credentials = {
+      access_key_id:     ENV.fetch('S3_ACCESS_KEY'),
+      secret_access_key: ENV.fetch('S3_SECRET_KEY'),
+      region:            ENV.fetch('S3_REGION'), # Required
+      stub_responses:    Rails.env.test? # Optional, avoid hitting S3 actual during tests
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
